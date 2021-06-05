c539445c5ffd55c59df39abc604195824680c047
Manisankar <ganapathymanisankar@gmail.com>
2021-06-05 18:39:36 +0530

    Adding the project - Core Java Training

---
+++ CoreJava_Training/src/Calculator.java
@@ -0,0 +1,15 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println("Arithmatic calculator");
+		System.out.println(args.length);
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		System.out.println("Addition: "+ (a+b));
+		System.out.println("Subraction: "+ (a-b));
+		System.out.println("Multiplication: "+ (a*b));
+		System.out.println("Division: "+ (a/b));
+	}
+
+}
